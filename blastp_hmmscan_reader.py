4e26b19f0febed8daa5a25f50a74d91dc7bd0109
munasheh <83253590+munasheh@users.noreply.github.com>
2025-07-13 19:27:00 -0400

    Week 9

---
+++ blastp_hmmscan_reader.py
@@ -0,0 +1,20 @@
+##make a table that contains the information provided in the txt files that you made for this assignment
+## I think the best way to do this would be to have the script search for headers in the different files
+##may also need to merge files
+## the table needs the following headers: name of the transcript in the longesr_orfs.pep file, length of the peptide, name of the best hit found by blastp, name of the sequence ID found by blastp, name of the first pfam domain found(some may not have this)
+
+
+##import modules
+import pandas as pd
+
+# tsv_file = input('please enter the name of the file you would like to turn into a table: ')
+# df = pd.read_csv(tsv_file)
+# print(df)
+
+# output_table = input('please enter the name of the file that you would like to output to: ')
+
+##need to make a txt file from a csv file. 
+
+##starting over here
+
+
